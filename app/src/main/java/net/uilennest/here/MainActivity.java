52eb7963e4e897622edaec05cddcea60b9bc6e9b
Nico Vermaas <vermaas@astron.nl>
2017-10-10 19:34:29 +0200

    Initial Commit

--- app/src/main/java/net/uilennest/here/MainActivity.java
+++ app/src/main/java/net/uilennest/here/MainActivity.java
@@ -17,9 +17,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+// e-mail
+import android.net.Uri;
+import android.content.Intent;
+import android.util.Log;
+
+// json
+import org.json.JSONException;
+import org.json.JSONObject;
+
+// http
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.IOException;
+import java.io.DataOutputStream;
+
 public class MainActivity extends Activity {
 
-    Button btnShowLocation;
+    Button buttonGetLocation;
+    Button buttonSendHome;
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
 
@@ -45,10 +63,10 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
 
-        btnShowLocation = (Button) findViewById(R.id.button);
+        buttonGetLocation = (Button) findViewById(R.id.buttonGetLocation);
 
         // show location button click event
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+        buttonGetLocation.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
@@ -73,5 +91,85 @@ public class MainActivity extends Activity {
 
             }
         });
+
+        buttonSendHome = (Button) findViewById(R.id.buttonSendHome);
+
+        // show location button click event
+        buttonSendHome.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(MainActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    String coordinates = Double.toString(longitude) + ',' + Double.toString(latitude);
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Sending Coordinates : "+coordinates,
+                            Toast.LENGTH_LONG).show();
+                    sendEmail(coordinates);
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+
+    protected void sendEmail(String content) {
+        Log.i("Send email", "");
+        String[] TO = {"nicomint@xs4all.nl"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Here! coordinates");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, content);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("email sent...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void postCoordinates(String content) {
+        try {
+            URL url = new URL("http://uilennest.net/owlsnest/hiking/tripdetails/1000/");
+            String type = "application/json";
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setRequestMethod("PUT");
+            httpURLConnection.setRequestProperty("Content-Type", type);
+            httpURLConnection.connect();
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("coordinates", content);
+
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+            wr.writeBytes(jsonObject.toString());
+            wr.flush();
+            wr.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+
+
+        }
     }
 }
