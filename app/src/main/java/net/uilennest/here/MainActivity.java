e8367ee849a1b2a39dcab28221bb30aa97ca537e
Nico Vermaas <vermaas@astron.nl>
2017-10-15 16:57:23 +0200

    adding put

--- app/src/main/java/net/uilennest/here/MainActivity.java
+++ app/src/main/java/net/uilennest/here/MainActivity.java
@@ -1,6 +1,10 @@
 package net.uilennest.here;
 
 import android.os.Bundle;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 // https://www.tutorialspoint.com/android/android_location_based_services.htm
 import android.Manifest;
@@ -10,6 +14,8 @@ import android.support.v4.app.ActivityCompat;
 import android.test.mock.MockPackageManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 // e-mail
@@ -23,26 +29,103 @@ import org.json.JSONObject;
 
 // http
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.io.IOException;
 import java.io.DataOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.accounts.AccountManager;
+import android.accounts.Account;
 
 public class MainActivity extends Activity {
 
     Button buttonGetLocation;
-    Button buttonSendHome;
+    Button buttonSendHomePost;
+    Button buttonSendHomePut;
+    EditText editName;
+    EditText editTitle;
+    EditText editDescription;
+    TextView textCoordinates;
+
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
 
     // GPSTracker class
+    public String HOMEBASE_URL = "http://uilennest.net/homebase/datacenter/locations/1/";
+    public String HTTP_METHOD = "POST";
+
+    // using globals because of threading
+    public String username = "username";
+    public String title = "sent by Here";
+    public String longitude = "unknown";
+    public String latitude = "unknown";
+    public String description = "I am here!";
     GPSTracker gps;
+
+
+    // read gmail username from the google acount
+    // this will not work on the emulator, which has no google accounts.
+    // Hence the default username of 'emulator'
+    public String getUsername(String defaultName) {
+        List<String> possibleEmails = null;
+        try {
+            AccountManager manager = AccountManager.get(this);
+            //Account[] accounts = manager.getAccountsByType("com.google");
+            Account[] accounts = manager.getAccounts();
+
+            for (Account account : accounts) {
+
+                // return the first gmail account username
+                if (account.name.contains("gmail.com")) {
+                    String[] parts = account.name.split("@");
+                    username = parts[0];
+                    return username;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+         }
+
+        return defaultName;
+    }
+
+    void doGetLocation() {
+        gps = new GPSTracker(MainActivity.this);
+
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+            // longitude = String.valueOf(gps.getLongitude());
+            longitude = String.format("%.5f", gps.getLongitude());
+            // latitude = String.valueOf(gps.getLatitude());
+            latitude = String.format("%.5f", gps.getLatitude());
+            textCoordinates = (TextView) findViewById(R.id.textCoordinates);
+            textCoordinates.setText("latitude = "+latitude+", longitude = "+longitude);
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+    }
+
+    void doShowLocation() {
+        Toast.makeText(getApplicationContext(), "Location is - \nLon: "+ longitude+ "\nLat: " + latitude, Toast.LENGTH_LONG).show();
+    }
+
+    void doSendCoordinates(String method) {
+        HTTP_METHOD = method;
+        Toast.makeText(getApplicationContext(), HTTP_METHOD+ " to HomeBase: \n"+latitude+", "+longitude, Toast.LENGTH_LONG).show();
+        sendCoordinates();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // attempt to update the username with the google account username.
+        username = getUsername(username);
 
         try {
             if (ActivityCompat.checkSelfPermission(this, mPermission)
@@ -58,114 +141,97 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
 
-        buttonGetLocation = (Button) findViewById(R.id.buttonGetLocation);
+        editName = (EditText) findViewById(R.id.editName);
+        editName.setText(username);
 
-        // show location button click event
+        editDescription = (EditText) findViewById(R.id.editDescription);
+        editDescription.setText(description);
+
+        buttonGetLocation = (Button) findViewById(R.id.buttonGetLocation);
         buttonGetLocation.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
                 // create class object
-                gps = new GPSTracker(MainActivity.this);
-
-                // check if GPS enabled
-                if(gps.canGetLocation()){
-
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
-                            + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                    gps.showSettingsAlert();
-                }
-
+                doGetLocation();
+                doShowLocation();
             }
         });
 
-        buttonSendHome = (Button) findViewById(R.id.buttonSendHome);
-
-        // show location button click event
-        buttonSendHome.setOnClickListener(new View.OnClickListener() {
+        buttonSendHomePost = (Button) findViewById(R.id.buttonSendHomePost);
+        buttonSendHomePost.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                // create class object
-                gps = new GPSTracker(MainActivity.this);
-
-                // check if GPS enabled
-                if(gps.canGetLocation()){
-
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-                    String coordinates = Double.toString(longitude) + ',' + Double.toString(latitude);
-
-                    Toast.makeText(getApplicationContext(), "Sending Coordinates : "+coordinates,
-                            Toast.LENGTH_LONG).show();
-                    // sendEmail(coordinates);
-                    postCoordinates(coordinates);
-                }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                    gps.showSettingsAlert();
-                }
-
+                doGetLocation();
+                HOMEBASE_URL = "http://uilennest.net/homebase/datacenter/locations/";
+                doSendCoordinates("POST"); //update existing
             }
         });
-    }
 
-    protected void sendEmail(String content) {
-        Log.i("Send email", "");
-        String[] TO = {"nicomint@xs4all.nl"};
-        String[] CC = {""};
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        if (username.equalsIgnoreCase("nicovermaas")) {
+            buttonSendHomePut = (Button) findViewById(R.id.buttonSendHomePut);
+            buttonSendHomePut.setOnClickListener(new View.OnClickListener() {
 
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Here! coordinates");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, content);
-
-        try {
-            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            finish();
-            Log.i("email sent...", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                @Override
+                public void onClick(View arg0) {
+                    doGetLocation();
+                    HOMEBASE_URL = "http://uilennest.net/homebase/datacenter/locations/1/";
+                    doSendCoordinates("PUT"); // create nieuw
+                }
+            });
         }
     }
 
-    protected void postCoordinates(String content) {
-        try {
-            URL url = new URL("http://uilennest.net/here");
-            String type = "application/json";
-            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setRequestMethod("PUT");
-            httpURLConnection.setRequestProperty("Content-Type", type);
-            httpURLConnection.connect();
 
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("coordinates", content);
+    public void sendCoordinates() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(HOMEBASE_URL);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod(HTTP_METHOD);
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
 
-            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
-            wr.writeBytes(jsonObject.toString());
-            wr.flush();
-            wr.close();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    //method 1
+                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
 
-        }
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("timestamp", sdf.format(timestamp));
+                    jsonParam.put("title", title);
+                    jsonParam.put("username", editName.getText().toString());
+                    jsonParam.put("description", editDescription.getText().toString());
+                    jsonParam.put("latitude", latitude);
+                    jsonParam.put("longitude", longitude);
+                    // http://maps.google.com/maps?q=24.197611,120.780512
+                    String google_map = "http://maps.google.com/maps?q="+latitude+","+longitude;
+                    jsonParam.put("url", google_map);
+
+                    Log.i("JSON", jsonParam.toString());
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 }
